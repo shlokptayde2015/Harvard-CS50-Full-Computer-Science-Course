f33ea35f6689f3beb0a6e32b9e5fd2bff94cd55e
shlokptayde2015 <161626368+shlokptayde2015@users.noreply.github.com>
2024-02-29 08:26:58 +0400

    Add files via upload

---
+++ parity.c
@@ -0,0 +1,21 @@
+#include <cs50>
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompting the "n" variable:
+    int n = get_int("n: ");
+
+    // Performing else-if statements:
+
+    //If n is even:
+    if(int n % 2 == 0) {
+        printf("even\n");
+
+        else {
+            printf("odd\n");
+        }
+    }     
+
+
+}
